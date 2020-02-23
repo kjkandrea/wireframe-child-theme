60099b26ca0d5816264896c70a281b39dea3f776
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-02-23 19:44:36 +0900

    scss 체계 도입 및 stylesheet 로드 재정의

--- functions.php
+++ functions.php
@@ -19,3 +19,22 @@ function wireframe_theme_widgets_init() {
 }
 
 add_action( 'widgets_init', 'wireframe_theme_widgets_init' );
+
+
+/**
+* Enqueue scripts and styles.
+*/
+function wireframe_theme_scripts() {
+    // First de-register the main stylesheet (which is now your child theme style.css)
+	wp_dequeue_style( WPEX_THEME_STYLE_HANDLE );
+	wp_deregister_style( WPEX_THEME_STYLE_HANDLE );
+
+	// Add the parent style.css with the main style handle
+	wp_enqueue_style( WPEX_THEME_STYLE_HANDLE, get_template_directory_uri() . '/style.css', array(), WPEX_THEME_VERSION );
+
+    wp_enqueue_style('spoqa-han-sans', 'https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css', array());
+
+    wp_enqueue_style('wireframe-stylesheet', get_stylesheet_directory_uri() . '/assets/css/wireframe.min.css', array( WPEX_THEME_STYLE_HANDLE ), '1.0.0');
+}
+
+add_action( 'wp_enqueue_scripts', 'wireframe_theme_scripts' );
